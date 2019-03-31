7dca5a085ccfb1b94f052c06764aa8515b6b6dca
iamtheyammer <32084206+iamtheyammer@users.noreply.github.com>
2019-03-31 08:48:19 -0700

    Notify on out of stock

--- index.js
+++ index.js
@@ -6,43 +6,69 @@ const notifyUrls = [
 
 exports.handler = (event, context, callback) => {
   axios({
-      method: 'get',
-      url: 'https://primenow.amazon.com/s?maxResults=10&k=oatly&ref=nb_sb_noss_1',
-      headers: {
-        'Cookie': process.env.COOKIE
+    method: 'get',
+    url: 'https://primenow.amazon.com/s?maxResults=10&k=oatly&ref=nb_sb_noss_1',
+    headers: {
+      'Cookie': process.env.COOKIE
+    }
+  })
+  .then((response) => {
+    const currentOatly = JSON.parse(process.env.ISOATLY);
+    if (response.data.noResults) {
+      if(currentOatly.length > 0) {
+        let oatlyString = currentOatly.map((item) => item.title).join(', ');
+        notifyUrls.forEach((url) => axios({
+          method: 'post',
+          url,
+          data: {
+            value1: 'OatlyChecker: ' + oatlyString + ' is now out of stock.',
+            value2: '',
+            value3: response.config.url
+          }
+        }).then(() => console.log('Success pulling IFTTT webhook')));
       }
-    })
-    .then((response) => {
-      if (response.data.noResults) {
-        process.env.ISOATLY = 'FALSE';
-        return callback('No oatly available.', null);
-      }
-      let oatlyNames = [];
-      let oatlyURL = '';
-      response.data.products.asinCards.forEach((product, index) => {
-        oatlyNames.push(product.title);
-        if(index === 0) oatlyURL = 'https://primenow.amazon.com' + product.productUrl;
-
-      });
-      let oatlyString = oatlyNames.join(', ');
-      if(process.env.ISOATLY === 'TRUE') return callback(null, {
-        statusCode: 201,
-        data: 'Already sent push, but the oatly is PRESENT!.'
-      })
-      notifyUrls.forEach((url) => axios({
-        method: 'post',
-        url,
-        data: {
-          value1: oatlyString,
-          value2: 'built by iamtheyammer: git.io/fjkGt'
-        }
-      }));
-      process.env.ISOATLY = 'TRUE';
-      return callback(null, {
-        statusCode: 200,
-        data: 'Got dem oatlies: ' + oatlyString
+      return callback('No oatly available.', null);
+    }
+    let oatlies = [];
+    response.data.products.asinCards.forEach((product, index) => {
+      oatlies.push({
+        asin: product.asin,
+        imgUrl: product.productImgSrc,
+        title: product.title,
+        url: 'https://primenow.amazon.com' + product.productUrl,
+        price: product.price.fullPrice
       });
     });
+    let oatlyTitles = oatlies.map((oatly) => oatly.title).join(', ');
+    if(process.env.ISOATLY === JSON.stringify(oatlies)) {
+      console.log({
+        statusCode: 201,
+        data: 'Already sent push, but the oatly is PRESENT!'
+      }, JSON.stringify(oatlies));
+      return callback(null, {
+        statusCode: 201,
+        data: 'Already sent push, but the oatly is PRESENT!'
+      })
+    }
+    notifyUrls.forEach((url) => axios({
+      method: 'post',
+      url,
+      data: {
+        value1: oatlyTitles,
+        value2: oatlies[0].imgUrl,
+        value3: request.config.url
+      }
+    })).then(() => console.log('Success pulling a webhook.'));
+    process.env.ISOATLY = JSON.stringify(oatlies);
+    console.log(JSON.stringify({
+      statusCode: 200,
+      data: 'Got dem oatlies: ' + oatlyTitles
+    }));
+    return callback(null, {
+      statusCode: 200,
+      data: 'Got dem oatlies: ' + oatlyTitles
+    });
+  });
 };
 
-process.env.TEST === 'TRUE' ? console.log(exports.handler(null, null, (err, data) => console.log(data))) : null;
+process.env.TEST === 'TRUE' ? console.log(exports.handler(null, null, (err, data) => console.log(err, data))) : null;
